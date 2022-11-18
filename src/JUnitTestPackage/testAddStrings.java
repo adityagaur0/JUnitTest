eaa8928aeae704a210e8164a6f6f7fe415303bc2
adityagaur0 <adityakumargaur2020@gmail.com>
2022-11-18 11:37:40 +0530

    JUnitTest

---
+++ src/JUnitTestPackage/testAddStrings.java
@@ -0,0 +1,17 @@
+package JUnitTestPackage;
+
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+public class testAddStrings {
+
+	@Test
+	public void test(){
+		jUnitFunctions junit = new jUnitFunctions();
+		int result = junit.addNumbers(100, 200);
+		assertEquals(300, result);
+		}
+
+}
